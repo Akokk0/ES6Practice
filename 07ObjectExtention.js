ce273638205f1de83ec2be5bb87ba39e498693b5
akokko <zhminecraft@sina.com>
2022-07-31 13:18:31 +0800

    Practice ObjectExtention and Export

---
+++ 07ObjectExtention.js
@@ -0,0 +1,28 @@
+const config = {
+    host: 'localhost',
+    port: 3306,
+    name: 'root',
+    password: 'root',
+    test: 'test'
+}
+
+const config2 = {
+    host: 'http://akokko.com',
+    port: 3307,
+    name: 'akokko',
+    password: 'qwer',
+    test2: 'test2'
+}
+
+// console.log(Object.is(config, config2))
+
+/*console.log(Object.is(122, 122))
+console.log(Object.is(NaN, NaN))
+console.log(NaN === NaN)
+console.log(NaN == NaN)*/
+
+// console.log(Object.assign(config, config2))
+
+/*
+console.log(Object.setPrototypeOf(config, config2))
+console.log(Object.getPrototypeOf(config))*/
---
+++ 08Export.js
@@ -0,0 +1,27 @@
+//分别暴露
+/*export const name = 'akokko'
+export function f() {
+    console.log('We can change you!')
+}
+*/
+
+//统一暴露
+/*const name = 'akokko'
+function f() {
+    console.log('We can change you!')
+}
+export {name, f}*/
+
+//默认暴露
+/*const name = 'akokko'
+function f() {
+    console.log('We can change you!')
+}
+export default {name, f}*/
+
+/*export default {
+    name: 'akokko',
+    f: () => {
+        console.log('We can change you!')
+    }
+}*/
